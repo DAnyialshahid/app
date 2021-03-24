7a88e828eeaba51bc84f5425481045261a84f5fe
Danyialshahid <Danyialshahid@yahoo.com>
2021-03-25 01:21:51 +0500

    Update head_top.php

--- views/theme3/head_top.php
+++ views/theme3/head_top.php
@@ -55,85 +55,18 @@
                     <a rel="nofollow" href="#"  onclick="$('#nav-mobile').toggle(700);"  data-activates="slide-out" class="button-collapse login-mob-nav btn btn-flat waves-effect waves-light"><i class="fa fa-arrow-left"></i></a>
                             </div>
         </li>
+            <li><a href="/" class="waves-effect waves-red">Home</a>    
                     <li>
                 <ul class="collapsible collapsible-accordion">
-                    <li><a class="collapsible-header  waves-effect waves-red">Top Stores</a>
+                      <li><a class="collapsible-header  waves-effect waves-red">Top Stores</a>
                         <div class="collapsible-body" style="">
                                     <ul> 
-          <ul class="collapsible collapsible-accordion"> 
-            <li><a class=" waves-effect waves-red">aa</a> 
-            <li><a class="collapsible-header waves-effect waves-red">Fashion</a> 
-              <div class="collapsible-body" style=""> 
-                <ul> 
-                  <li><a href="/store/jabong-coupons/">Jabong</a></li>
-                  <li><a href="/myntra-coupons/">Myntra</a></li>
-                  <li><a href="/abof-coupons/">Abof</a></li>
-                  <li><a href="/ajio-coupons-offers/">Ajio</a></li>
-                                                                         <li><a href="/reliance-trends-coupons-offers-online-shopping/">Reliance Trends</a></li>
-                  <li><a href="/store/zivame-coupons/">Zivame</a></li>
-                </ul> 
-              </div> 
-            </li>
-            <li><a class="collapsible-header waves-effect waves-red">Electronics</a> 
-              <div class="collapsible-body" style=""> 
-                <ul> 
-                  <li><a href="/store/flipkart-coupons/">Flipkart</a></li>
-                  <li><a href="/store/amazon-coupons/">Amazon</a></li>
-                  <li><a href="/store/snapdeal-coupons/">Snapdeal</a></li>
-                  <li><a href="/store/ebay-coupons/">Ebay</a></li>
-                  <li><a href="/store/shopclues-coupons/">Shopclues</a></li>
-                  <li><a href="/store/paytm-coupons/">Paytm</a></li>
-                  <li><a href="/store/tata-cliq-coupons-code-offers/">Tata Cliq</a></li>
-                </ul> 
-              </div> 
-            </li>
-            <li><a class="collapsible-header waves-effect waves-red">Recharge</a> 
-              <div class="collapsible-body" style=""> 
-                <ul> 
-                  <li><a href="/store/paytm-coupons/">Paytm</a></li>
-                  <li><a href="/store/freecharge-coupons-promo-code-offers-today/">Freecharge</a></li>
-                  <li><a href="/store/mobikwik-coupons/">Mobikwik</a></li>
-                  <li><a href="/store/airtel-coupons/">Airtel</a></li>
-                </ul> 
-              </div> 
-            </li>
-            <li><a class="collapsible-header waves-effect waves-red">Travel</a> 
-              <div class="collapsible-body" style=""> 
-                <ul> 
-                  <li><a href="/store/makemytrip-coupons/">Makemytrip</a></li>
-                  <li><a href="/store/yatra-coupons/">Yatra</a></li>
-                  <li><a href="/store/oyo-rooms-coupons/">Oyo Rooms</a></li>
-                  <li><a href="/store/air-bnb-coupons/">Air Bnb</a></li>
-                  <li><a href="/store/ola-cabs-coupons/">Ola</a></li>
-                  <li><a href="/store/uber-travels-coupons/">Uber</a></li>
-                  <li><a href="/store/zoomcar-coupons/">Zoom Car</a></li>
-                </ul> 
-              </div> 
-            </li>
-            <li><a class="collapsible-header waves-effect waves-red">Food &amp; Groceries</a> 
-              <div class="collapsible-body" style=""> 
-                <ul> 
-                  <li><a href="/dominos-coupons/">Dominos</a></li>
-                  <li><a href="/store/foodpanda-coupons/">Foodpanda</a></li>
-                  <li><a href="/store/nearbuy-coupons/">Nearbuy</a></li>
-                  <li><a href="/store/pizza-hut-coupons/">Pizzahut</a></li>
-                  <li><a href="/store/mcdonalds-coupons/">Mc Donalds</a></li>
-                  <li><a href="/store/swiggy-coupons/">Swiggy</a></li>
-                  <li><a href="/store/grofers-coupons/">Grofers</a></li>
-                </ul> 
-              </div> 
-            </li>
-            <li><a class="collapsible-header waves-effect waves-red">Home</a> 
-              <div class="collapsible-body" style=""> 
-                <ul> 
-                  <li><a href="/store/pepperfry-coupons/">Pepperfry</a></li>
-                                    <li><a href="/store/fabfurnish-coupons/">Fabfurnish</a></li>
-                </ul> 
-              </div> 
-            </li>
-          </ul>
-      </ul>
-                        </div>
+                <ul class="collapsible collapsible-accordion"> 
+                         <li><a href="/stores" class=" waves-effect waves-red"  >View All</a> </li>
+                         <li><a :href="'/store/'+store.custom_url" class=" waves-effect waves-red"  v-for="store in popular_stores.slice(0, 6)">{{store.name}}</a> </li>
+                         </ul>
+                  </ul>
+                              </div>
                     </li>
                 </ul>
             </li>
@@ -142,86 +75,22 @@
                     <li><a class="collapsible-header  waves-effect waves-red">Top Categories</a>
                         <div class="collapsible-body" style="">
                             <ul> 
-          <ul class="collapsible collapsible-accordion">          
-          <li><a class="collapsible-header waves-effect waves-red">Fashion</a> 
-          <div class="collapsible-body" style=""> 
-              <ul>
-                <li><a href="/clothing-offers-coupons/">Clothing</a></li>
-                <li><a href="/footwear-offers/">Footwear</a></li>
-                <li><a href="/watches-offers/">Watches</a></li>
-                <li><a href="/sunglasses-offers/">Sunglasses</a></li>
-                <li><a href="/bags-belts-wallets-offers/">Bags, Belts &amp; Wallets</a></li>
-                <li><a href="/online-jewellery-offers-coupons/">Jewellery &amp; Accessories</a></li>
-                <li><a href="/lingerie-offers-coupons/">Lingerie</a></li>
-              </ul>
-          </div>
+          <ul class="collapsible collapsible-accordion" >          
+    
+               <li><a href="/categories" class="collapsible-header waves-effect waves-red"  >View All</a> </li>
+               <li><a :href="'/category/'+cat.custom_url" class="collapsible-header waves-effect waves-red"  v-for="cat in popular_categories.slice(0, 6)">{{cat.name}}</a> </li>
           </li>
-          <li><a class="collapsible-header waves-effect waves-red">Electronics</a> 
-          <div class="collapsible-body" style="">
-              <ul>
-                <li><a href="/mobile-offers-coupons/">Mobiles</a></li>
-                <li><a href="/laptop-offers/">Laptops &amp; Computers</a></li>
-                <li><a href="/camera-offers/">Camera &amp; Accessories</a></li>
-                <li><a href="/mobile-computer-accessories-offers-coupons/">Mobile Accessories</a></li>
-                <li><a href="/kitchen-and-large-appliances-offers/">Large Appliances</a></li>
-              </ul>
-          </div>
-          </li>
-          <li><a class="collapsible-header waves-effect waves-red">Recharge</a> 
-          <div class="collapsible-body" style=""> 
-              <ul>
-                <li><a href="/mobile-recharge-offers/">Mobile Recharge</a></li>
-                <li><a href="/online-bill-payment-offers/">Bill Payment</a></li>
-                <li><a href="/data-card-recharge-offers/">Data Card Recharge</a></li>
-                <li><a href="/dth-recharge-offers/">DTH Recharge</a></li>
-                <li><a href="/metro-card-recharge-offers-coupons/">Metro Card Recharge</a></li>
-              </ul>
-          </div>
-          </li>
-          <li><a class="collapsible-header waves-effect waves-red">Travel</a> 
-          <div class="collapsible-body" style=""> 
-              <ul>
-                <li><a href="/domestic-flight-offers-coupons/">Domestic Flight Tickets</a></li>
-                <li><a href="/international-flight-booking-offers-coupons/">International Flight Tickets</a></li>
-                <li><a href="/bus-ticket-booking-online-offers-coupons/">Bus Tickets</a></li>
-                <li><a href="/cab-offers/">Car Rentals</a></li>
-                <li><a href="/hotels-booking-offers/">Hotels</a></li>
-                <li><a href="/holiday-travel-packages-offers/">Holiday Package</a></li>
-                <li><a href="/train-ticket-offers/">Train Ticket</a></li>
-              </ul>
-          </div>
-          </li>
-          <li><a class="collapsible-header waves-effect waves-red">Bank Offers</a> 
-          <div class="collapsible-body" style=""> 
-              <ul>
-                <li><a href="/master-maestro-card-offers/">Master &amp; Maestro Card Offers</a></li>
-                <li><a href="/sbi-offers/">SBI Offers</a></li>
-                <li><a href="/axis-bank-offers/">Axis Bank Offers</a></li>
-                <li><a href="/icici-bank-offers/">ICICI Bank Offers</a></li>
-                <li><a href="/citi-bank-offers/">CITI Bank Offers</a></li>
-                <li><a href="/hdfc-bank-offers/">HDFC Bank Offers</a></li>
-                <li><a href="/hsbc-bank-offers/">HSBC Bank Offers</a></li>
-                <li><a href="/rupay-card-offers/">Rupay Card Offers</a></li>
-                <li><a href="/bank-offers/">View All Bank Offers</a></li>
-              </ul>
-          </div>
-          </li>
-          <li><a class="collapsible-header waves-effect waves-red">Food &amp; Grocery</a> 
-          <div class="collapsible-body" style=""> 
-              <ul>
-                <li><a href="/restaurants-offers-deals/">Restaurants Offers</a></li>
-                <li><a href="/pizza-offers/">Pizza</a></li>
-                <li><a href="/grocery-offers-coupons/">Grocery</a></li>
-                <li><a href="/drinks-beverages-offers-coupons/">Drinks &amp; Beverages</a></li>
-              </ul>
-          </div>
-          </li>
+          
           </ul>
         </ul>
                         </div>
                     </li>
                 </ul>
             </li>
+                    <li><a href="/pages/contact_us" class="waves-effect waves-red">Contacts Us</a>    
+                    <li><a href="/page/about_us" class="waves-effect waves-red">About Us</a>    
+                    <li><a href="/pages/faq" class="waves-effect waves-red">Help</a>    
+                    <li><a href="/page/submit_offer" class="waves-effect waves-red">Add offer</a>    
                     </div>
 </ul>
  
